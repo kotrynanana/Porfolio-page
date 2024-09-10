8965cce02b89eca201f39e8c3a65658678c42269
Kotryna Brazaityte <kotryn.brazaityte@gmail.com>
2024-09-10 19:05:26 +0300

    add nav blur on scroll

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -43,3 +43,13 @@ window.onload = function() {
           });
   });
 }
+
+
+window.addEventListener('scroll', function() {
+  let navbar = document.getElementById('nav');
+  if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+  } else {
+      navbar.classList.remove('scrolled');
+  }
+});
