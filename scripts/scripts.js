95347694cdfe8d061e818c9f3947ee9938d04658
Kotryna Brazaityte <kotryn.brazaityte@gmail.com>
2024-09-11 22:49:05 +0300

    files format update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,79 +5,79 @@ let messageInput = document.getElementById("message");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        /* Toggle between hiding and showing the active panel */
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
 
-(function() {
-  // https://dashboard.emailjs.com/admin/account
-  emailjs.init({
-    publicKey: "cV3XHy135C6HSritr",
-  });
+(function () {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+        publicKey: "cV3XHy135C6HSritr",
+    });
 })();
 
-window.onload = function() {
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
-      event.preventDefault();
-      // these IDs from the previous steps
-      emailjs.sendForm('service_3k9xv78', 'template_rc49krr', this)
-          .then(() => {
-              console.log('SUCCESS!');
-              nameInput.value = '';
-              emailInput.value = '';
-              messageInput.value = '';
-              alert('Your message has been sent successfully');
-          }, (error) => {
-              console.log('FAILED...', error);
-          });
-  });
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('service_3k9xv78', 'template_rc49krr', this)
+            .then(() => {
+                console.log('SUCCESS!');
+                nameInput.value = '';
+                emailInput.value = '';
+                messageInput.value = '';
+                alert('Your message has been sent successfully');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
 }
 
 
-window.addEventListener('scroll', function() {
-  let navbar = document.getElementById('nav');
-  if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
-  } else {
-      navbar.classList.remove('scrolled');
-  }
+window.addEventListener('scroll', function () {
+    let navbar = document.getElementById('nav');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
 });
 
-document.getElementById('nav-works').addEventListener('click', function(event) {
-  event.preventDefault();
-  const targetId = this.getAttribute('href').substring(1);
-  const targetElement = document.getElementById(targetId);
-  const offset = 80;
-  const elementPosition = targetElement.getBoundingClientRect().top;
-  const offsetPosition = elementPosition + window.pageYOffset - offset;
+document.getElementById('nav-works').addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    const offset = 80;
+    const elementPosition = targetElement.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - offset;
 
-  window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth'
-  });
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
 });
 
-document.querySelector('a[href="#header"]').addEventListener('click', function(event) {
-  event.preventDefault();
-  const targetId = this.getAttribute('href').substring(1);
-  const targetElement = document.getElementById(targetId);
-  const offset = 200; 
-  const elementPosition = targetElement.getBoundingClientRect().top;
-  const offsetPosition = elementPosition + window.pageYOffset - offset;
+document.querySelector('a[href="#header"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    const offset = 200;
+    const elementPosition = targetElement.getBoundingClientRect().top;
+    const offsetPosition = elementPosition + window.pageYOffset - offset;
 
-  window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth'
-  });
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
 });
