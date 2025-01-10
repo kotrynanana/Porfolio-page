a39018477c7de6d729bd5b04d8fb9c80dbd21bf9
Kotryna Brazaityte <kotryn.brazaityte@gmail.com>
2025-01-10 17:02:35 +0200

    Navigation responsive all pages

--- scripts/works-scripts.js
+++ scripts/works-scripts.js
@@ -88,3 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
